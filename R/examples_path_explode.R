0c682e49d0d7c28d2408ad6b60e5185e945cd982
EvaMaeRey <evangeline.mae@gmail.com>
2021-07-09 23:07:06 -0400

    seems to be working

--- R/examples_path_explode.R
+++ R/examples_path_explode.R
@@ -3,7 +3,7 @@ write_flipbook_setup <- function(){
 "
 ---
 
-`r chunk_reveal('funct', title = 'funct')`
+`r flipbookr::chunk_reveal('funct', title = 'funct')`
 
 ```{r funct, include = F}
 code
@@ -18,23 +18,22 @@ examples_path_flipbooks <- function(path = "R/", flipbook_setup = write_flipbook
 
 fs::dir_ls(path, pattern = "\\.R") %>%
   tibble::tibble(file = .) %>%
-  dplyr::mutate(file = as.character(file)) %>%
-  dplyr::mutate(funct = stringr::str_remove_all(file, paste0(path, "|\\.R"))) %>%
+  dplyr::mutate(file = as.character(.data$file)) %>%
+  dplyr::mutate(funct = stringr::str_remove_all(.data$file, paste0(path, "|\\.R"))) %>%
   dplyr::mutate(text = purrr::map(file, readLines)) %>%
   tidyr::unnest() %>%
-  dplyr::filter(text %>% stringr::str_detect("^\\#\\' ")) %>%
-  dplyr::group_by(file, funct) %>%
-  dplyr::mutate(start_example = text %>% stringr::str_detect("^\\#\\' @examples")) %>%
-  dplyr::mutate(example = ifelse(start_example, T, NA)) %>%
-  tidyr::fill(example) %>%
-  dplyr::filter(example, !start_example) %>%
-  dplyr::mutate(text = text %>% stringr::str_remove("^\\#\\' ")) %>%
-  dplyr::summarise(text = paste(text, collapse = "\n")) %>%
+  dplyr::filter(.data$text %>% stringr::str_detect("^\\#\\' ")) %>%
+  dplyr::group_by(.data$file, .data$funct) %>%
+  dplyr::mutate(start_example = .data$text %>% stringr::str_detect("^\\#\\' @examples")) %>%
+  dplyr::mutate(example = ifelse(.data$start_example, T, NA)) %>%
+  tidyr::fill(.data$example) %>%
+  dplyr::filter(.data$example, !.data$start_example) %>%
+  dplyr::mutate(text = .data$text %>% stringr::str_remove("^\\#\\' ")) %>%
+  dplyr::summarise(text = paste(.data$text, collapse = "\n")) %>%
   dplyr::mutate(breaks = flipbook_setup) %>%
-  dplyr::mutate(breaks = stringr::str_replace_all(breaks, "funct", funct)) %>%
-  dplyr::mutate(breaks = stringr::str_replace(breaks, "code", text)) %>%
-  dplyr::pull(breaks) %>%
-  cat()
+  dplyr::mutate(breaks = stringr::str_replace_all(.data$breaks, "funct", .data$funct)) %>%
+  dplyr::mutate(breaks = stringr::str_replace(.data$breaks, "code", .data$text)) %>%
+  dplyr::pull(breaks)
 
 }
 
@@ -44,7 +43,7 @@ write_xaringan_yaml <- function(){
 '---
 title: "Example Exploder"
 subtitle: "With flipbookr and xaringan"
-author: "You!"
+author: ""
 output:
   xaringan::moon_reader:
     lib_dir: libs
@@ -56,6 +55,37 @@ output:
       countIncrementalSlides: false
 ---'
 
+}
 
+#'
+#'
+#' @param path
+#' @param flipbook_setup
+#' @param yaml
+#' @param rmd
+#' @param render
+#'
+#' @return
+#' @export
+#'
+#' @examples
+examples_path_write_exploded <- function(path = "R/",
+                                    flipbook_setup = write_flipbook_setup(),
+                                    yaml = write_xaringan_yaml(),
+                                    rmd = "docs/exploded_examples.Rmd",
+                                    render = T){
+
+  examples_flipbook <- examples_path_flipbooks(path, flipbook_setup = flipbook_setup)
+
+  paste(yaml, examples_flipbook, sep = "\n\n") %>%
+    writeLines(con = rmd)
+
+  if(render){
+
+    rmarkdown::render(input = rmd)
+
+  }
 
 }
+
+
---
+++ R/one_plus_one.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' one_plus_one() ->
+#' adding
+#' adding + one_plus_one()
+one_plus_one <- function(){
+
+  1 + 1
+
+}
