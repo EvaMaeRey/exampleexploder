4cdac7eca7f1fd56eb3ba1f28f68a0a778720952
EvaMaeRey <evangeline.mae@gmail.com>
2021-07-11 14:00:06 -0400

    added set up code chunk, made package name argument for yaml

--- R/examples_path_explode.R
+++ R/examples_path_explode.R
@@ -3,7 +3,7 @@ write_flipbook_setup <- function(){
 "
 ---
 
-`r flipbookr::chunk_reveal('funct', title = 'funct')`
+`r flipbookr::chunk_reveal('funct', title = '### funct')`
 
 ```{r funct, include = F}
 code
@@ -14,12 +14,13 @@ code
 
 
 
-examples_path_flipbooks <- function(path = "R/", flipbook_setup = write_flipbook_setup()){
+examples_path_flipbooks <- function(package_path = ".",
+  functions_path = "./R/", flipbook_setup = write_flipbook_setup()){
 
-fs::dir_ls(path, pattern = "\\.R") %>%
+fs::dir_ls(functions_path, pattern = "\\.R") %>%
   tibble::tibble(file = .) %>%
   dplyr::mutate(file = as.character(.data$file)) %>%
-  dplyr::mutate(funct = stringr::str_remove_all(.data$file, paste0(path, "|\\.R"))) %>%
+  dplyr::mutate(funct = stringr::str_remove_all(.data$file, paste0(functions_path, "|\\.R"))) %>%
   dplyr::mutate(text = purrr::map(file, readLines)) %>%
   tidyr::unnest() %>%
   dplyr::filter(.data$text %>% stringr::str_detect("^\\#\\' ")) %>%
@@ -38,12 +39,13 @@ fs::dir_ls(path, pattern = "\\.R") %>%
 }
 
 
-write_xaringan_yaml <- function(){
+write_xaringan_yaml <- function(package_name){
 
+paste0(
 '---
-title: "Example Exploder"
-subtitle: "With flipbookr and xaringan"
-author: ""
+title: ', package_name,'
+subtitle: ""
+author: "Exploded Examples"
 output:
   xaringan::moon_reader:
     lib_dir: libs
@@ -54,8 +56,23 @@ output:
       highlightLines: true
       countIncrementalSlides: false
 ---'
+)
 
 }
+
+write_setup_code_chunk <- function(package_name){
+
+paste0(
+'```{r setup, echo = F}
+knitr::opts_chunk$set(echo = TRUE, message = F, warning = F, fig.height = 6, comment = "")
+library(',
+package_name
+,')
+```')
+
+}
+
+
 
 #'
 #'
@@ -69,15 +86,20 @@ output:
 #' @export
 #'
 #' @examples
-examples_path_write_exploded <- function(path = "R/",
+examples_path_write_exploded <- function(package_name,
+                                        package_path = ".",
+                                         functions_path = "./R/",
                                     flipbook_setup = write_flipbook_setup(),
-                                    yaml = write_xaringan_yaml(),
+                                    yaml = write_xaringan_yaml(package_name),
+                                    setup_code_chunk = write_setup_code_chunk(package_name),
                                     rmd = "docs/exploded_examples.Rmd",
                                     render = T){
 
-  examples_flipbook <- examples_path_flipbooks(path, flipbook_setup = flipbook_setup)
+  examples_flipbook <- examples_path_flipbooks(functions_path = functions_path,
+                                               flipbook_setup = flipbook_setup)
 
-  paste(yaml, examples_flipbook, sep = "\n\n") %>%
+  paste(examples_flipbook, collapse = "\n") %>%
+  paste(yaml, setup_code_chunk, ., sep = "\n\n") %>%
     writeLines(con = rmd)
 
   if(render){
---
+++ R/two_plus_two.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' two_plus_two()
+#' two_plus_two() + one_plus_one()
+two_plus_two <- function(){
+
+  2 + 2
+
+}
